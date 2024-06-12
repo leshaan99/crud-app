75adafdb407243132680512deaae66ffdc7b3da7
Leshan Sanjeewa <143873948+leshan99@users.noreply.github.com>
2024-06-12 19:08:55 +0530

    Create connection with mysql

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,10 @@
 const express = require("express");
+const cors = require("cors");
+const mysql = require("mysql");
+
 const app = express();
+
+app.use(cors());
 
 app.listen(8081, () => {
     console.log("listening");
