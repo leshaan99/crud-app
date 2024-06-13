ec1a834b10acb0bb44a17fc983c31fd514eed490
Leshan Sanjeewa <143873948+leshan99@users.noreply.github.com>
2024-06-13 19:01:36 +0530

    Fetch the data from backend

--- frontend/src/Student.js
+++ frontend/src/Student.js
@@ -1,10 +1,27 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import axios from 'axios'
+import './Student.css'
 
 const Student = () => {
+
+    useEffect(() => {
+        axios.get('http://localhost:1000/')
+        .then(res => console.log(res))
+        .catch(err => console.log(err));
+    }, [])
+
   return (
-    <div className='student container'>
-      <div className='student content'>
-        <button className='add-btn'>Add +</button>
+    <div className="student-container">
+      <div className="student-content">
+        <button className="add-btn">Add +</button>
+        <table className="table">
+            <thrad>
+                <tr>
+                    <th>Name</th>
+                    <th>Email</th>
+                </tr>
+            </thrad>
+        </table>
       </div>
     </div>
   )
