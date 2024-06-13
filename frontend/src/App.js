9e8554cb5227c0c27f576885f465e3eb4507216b
Leshan Sanjeewa <143873948+leshan99@users.noreply.github.com>
2024-06-13 18:42:56 +0530

    Create preview and add css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
-import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Student from './Student';
 
 function App() {
   return (
---
+++ frontend/src/Student.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Student = () => {
+  return (
+    <div className='student container'>
+      <div className='student content'>
+        <button className='add-btn'>Add +</button>
+      </div>
+    </div>
+  )
+}
+
+export default Student
